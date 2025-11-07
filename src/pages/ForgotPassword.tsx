578656d73f31fe3e842b2b5d2ac5f50e80426398
özgür çelik <67160118+ozgurcelik40@users.noreply.github.com>
2025-11-07 10:58:38 +0300

    Updated ForgotPassword.tsx

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -41,8 +41,8 @@ export default function ForgotPassword() {
               <div className="flex items-center space-x-2">
                 <GraduationCap className="w-8 h-8 text-blue-600" />
                 <span className="text-2xl font-bold">
-                  <span className="text-gray-900">Vocation</span>
-                  <span className="text-blue-600">English</span>
+                  <span className="text-gray-900">Mesleki</span>
+                  <span className="text-blue-600">İngilizce</span>
                 </span>
               </div>
             </div>
